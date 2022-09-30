870f11df49c7013e866d0a654c33da9d5feaefc2
Aaron Stockdale <shadowstock76@gmail.com>
2022-09-30 17:39:29 -0400

    Final Product. Cleaned up with CSS to finish

--- main.js
+++ main.js
@@ -1,28 +1,43 @@
 const customerContainer = document.querySelector(".customerDiv");
-console.log('Hello world');
 
 function busDirectory(customerArray) {
-    for (let customer of customerArray) {
-        let peopleDiv = document.createElement("div");
-        let name = document.createElement("h1");
-        let email = document.createElement ("h4");
-        let location= document.createElement ("p");
-        let dob = document.createElement ("p");
-        let registered = document.createElement ("p");
+  for (let customer of customerArray) {
+    let peopleDiv = document.createElement("div");
+    let picture = document.createElement("img");
+    let name = document.createElement("h1");
+    let email = document.createElement("h4");
+    let location = document.createElement("p");
+    let dob = document.createElement("p");
+    let registered = document.createElement("p");
 
-        name.innerText = `${customer.name.title} ${customer.name.first} ${customer.name.last}`;
-        email.innerText = customer.email;
-        location.innerText = `${customer.location.street.number} ${customer.location.street.name}\n${customer.location.city} ${customer.location.state} ${customer.location.country} ${customer.location.postcode}`;
-        dob.innerText = customer.dob.date;
-        registered.innerText = `Customer since: ${customer.registered.date}`;
-        
-        customerContainer.appendChild(peopleDiv);
-        peopleDiv.appendChild(name);
-        peopleDiv.appendChild(email);
-        peopleDiv.appendChild(location);
-        peopleDiv.appendChild(dob);
-        peopleDiv.appendChild(registered);
-    }
+    picture.src = `${customer.picture.large}`;
+    name.innerText = `${customer.name.title} ${customer.name.first} ${customer.name.last}`;
+    email.innerText = customer.email;
+    location.innerText = `${customer.location.street.number} ${
+      customer.location.street.name
+    }\n${customer.location.city} ${nameToAbbr(customer.location.state)} ${
+      customer.location.postcode
+    }`;
+    dob.innerText = `DOB: ${moment(customer.dob.date).format("MMMM Do YYYY")}`;
+    registered.innerText = `Customer since: ${moment(
+      customer.registered.date
+    ).format("MMMM Do YYYY")}`;
+
+    customerContainer.appendChild(peopleDiv);
+    peopleDiv.appendChild(picture);
+    peopleDiv.appendChild(name);
+    peopleDiv.appendChild(email);
+    peopleDiv.appendChild(location);
+    peopleDiv.appendChild(dob);
+    peopleDiv.appendChild(registered);
+
+    peopleDiv.classList.add("peopleDiv");
+    picture.classList.add("picture");
+    name.classList.add("name");
+    location.classList.add("location");
+    email.classList.add("eMail");
+    picture.classList.add("picture");
+  }
 }
 
 busDirectory(customers);
